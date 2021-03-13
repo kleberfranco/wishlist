b90db31bda222553bc8312544c2eb97d9c769d5c
Kleber Franco <kleber_franco@hotmail.com>
2021-03-12 23:08:52 -0300

    Testes unitários

--- api/src/__tests__/authController.test.js
+++ api/src/__tests__/authController.test.js
@@ -1,18 +1,28 @@
 const server = require('../app')
 const supertest = require('supertest')
-const app = new server()
+const serverapp = new server()
+const app = serverapp.server
 const request = supertest(app)
 
 describe('Post Endpoints', () => {
-    it('should create a new post', async () => {
-        const res = await request()
+    it('should create a new post', async done => {
+        const res = await request
             .post('/auth/token')
+            .set('Content-Type', 'application/json')
             .send({
-                uesr: 'testes',
+                user: 'testes',
                 role: 'app',
             })
-        // expect(res.statusCode).toEqual(201)
+        // expect(res.statusCode).toEqual(200)
         // expect(res.body).toHaveProperty('post')
         done()
     })
 })
+
+// it('Gets the test endpoint', async done => {
+//     // Sends GET Request to /test endpoint
+//     const res = await request.get('/customers')
+//
+//     // ...
+//     done()
+// })
